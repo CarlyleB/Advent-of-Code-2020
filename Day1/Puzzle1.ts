dd4cbf736a2749fa0d299265149edaee8057e7b0
Carly Burroughs <burroughs.carlyle@gmail.com>
2020-12-02 23:08:30 -0500

    separated findMatch function

--- Day1/Puzzle1.ts
+++ Day1/Puzzle1.ts
@@ -6,10 +6,11 @@ const file = readFileSync(process.argv[2], 'utf-8');
 const entries: Array<number> = file.split('\n').map((x) => parseInt(x));
 
 const evalEntries = (items: Array<number>): number => {
-    const item = items.find((x, idx) => {
+    const findMatch = (x: number, idx: number) => {
         return items.slice(idx + 1).find((y) => x + y === GOAL) !== undefined;
-    });
-    return item ? item * (GOAL - item) : -1;
+    }
+    const item = items.find(findMatch);
+    return (item !== undefined) ? item * (GOAL - item) : -1;
 }
 
 console.log(evalEntries(entries));
